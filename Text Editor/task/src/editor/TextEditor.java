8152f81e8f2334bef408a2d14c516d4320977b82
NJBodam <bodambest@yahoo.com>
2022-04-23 20:02:37 +0100

    added texfield

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -5,8 +5,21 @@ import javax.swing.*;
 public class TextEditor extends JFrame {
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(600, 600);
         setVisible(true);
         setLayout(null);
+        setTitle("The first stage");
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        JLabel nameLabel = new JLabel("Your Name");
+        nameLabel.setBounds(40,20, 100,30);
+        add(nameLabel);
+
+        JTextField nameTextField = new JTextField();
+        nameTextField.setBounds(140,20, 120,30);
+        add(nameTextField);
     }
 }
